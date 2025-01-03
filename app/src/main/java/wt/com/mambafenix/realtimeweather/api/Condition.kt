af2d79ecd91d08423206c8d2046d5b790b8ac7e4
jeff_ <jeffreyazcarate@gmail.com>
2025-01-03 13:29:30 -0500

    Hijo mio

---
+++ app/src/main/java/wt/com/mambafenix/realtimeweather/api/Condition.kt
@@ -0,0 +1,7 @@
+package wt.com.mambafenix.realtimeweather.api
+
+data class Condition(
+    val code: String,
+    val icon: String,
+    val text: String
+)
---
+++ app/src/main/java/wt/com/mambafenix/realtimeweather/api/Constant.kt
@@ -0,0 +1,6 @@
+package wt.com.mambafenix.realtimeweather.api
+
+object Constant {
+
+    val apiKey = "d893bf11b6c54d5e9fa183016242712"
+}
